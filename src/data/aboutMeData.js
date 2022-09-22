ab24271b03324c5326e707559e3c4263d2066345
salem <salem.dahmani345@gmail.com>
2022-09-22 20:56:49 +0100

    first commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio : 'My name is salem dahmani i graduated 2019 in computer science and i started busineess intelligence degree in IHEC , since i was in high school developped alot of mini games using pascal and i started the web development path at college when i discovered react , in my free time i usually learn css tricks and new javascripts technique to make my website faster and look smooth .'
+			},
+	{
+		id: 2,
+		bio: 'Throughout the year I developed a sense of joy in using my academic and professional skills in my self-improvement journey. I firmly believe that my professional path is a highway to personal growth, this explains my thirst for knowledge. This thirst fuels my discipline, curiosity, authenticity, and motivations.',
+	},
+];
